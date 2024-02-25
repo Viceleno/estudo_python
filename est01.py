87e4c93f0ba64bf07c1899764f4f942b8d897fcd
Viceleno Barros <vicelenoti@gmail.com>
2024-02-25 15:24:47 -0300

    1º Commit - aprendendo variaveis e tipos

---
+++ est01.py
@@ -0,0 +1,23 @@
+
+
+print("Hello, world!")
+
+
+nome = 'Viceleno'
+sobrenome = 'Barros'
+idade = 41
+ano_nascimento = 1982
+maior_de_idade = idade >= 18
+altura_metros = 1.90
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
+
+
+# testando o comentário
+
+''' comentários de várias
+linhas (DocString)'''
