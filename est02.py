2fe11dde26474d8a8a7be784e3b9a96985bf3ad6
Viceleno Barros <vicelenoti@gmail.com>
2024-02-26 15:21:42 -0300

    3º Commit - formatação de numeros

--- est02.py
+++ est02.py
@@ -4,5 +4,6 @@ peso = 92
 imc = peso/(altura*altura)
 print('Seu nome é: ', nome)
 print('Voce mede: ', altura, ' metros.')
-print('Seu peso é: ', peso, 'Kggit')
+print('Seu peso é: ', peso, 'Kg')
 print('Seu imc é: ', imc)
+print('Seu imc é: ', f'{imc:.2f}')
