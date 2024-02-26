daa136d57132ddab6d70823591056495f2621729
Viceleno Barros <vicelenoti@gmail.com>
2024-02-26 15:12:41 -0300

    2º Commit - inserindo estudos de calculo

---
+++ est02.py
@@ -0,0 +1,8 @@
+nome = 'Viceleno'
+altura = 1.90
+peso = 92
+imc = peso/(altura*altura)
+print('Seu nome é: ', nome)
+print('Voce mede: ', altura, ' metros.')
+print('Seu peso é: ', peso, 'Kggit')
+print('Seu imc é: ', imc)
